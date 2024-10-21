b61f5868684b46494d7efc3e58825c5d5ce64e6c
Tazzakka Arif <Tazzakka Arif@Aptechnk1.edu>
2024-10-21 11:06:20 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,159 @@
+// var array=["iphone","samsung","nokia"];
+
+// var array1=["iphone","samsung","nokia"];
+// array.push("motoroola");
+// console.log(array);
+// array.pop();
+// console.log(array);
+// array.shift();
+// console.log(array);
+
+// array.unshift("oppo");
+// console.log(array);
+
+// var mainarray=array.concat(array1);
+// console.log(mainarray);
+
+
+// var slicekrol=array.slice(1)
+// console.log(slicekrol);
+
+
+
+// var u_email=prompt("enter email");
+
+//   var checkemail=obj.find(e=> e.email===u_email)
+//   if(checkemail){
+
+//        console.log("already exist");
+
+
+//   }
+
+//   else{
+
+//    obj = [...obj, { id: 4, email: u_email }]
+
+
+//   }
+// console.log(checkemail);
+// var obj=[
+
+//     { id:1,name:"tazzakka arif",email:"abc@gmail.com",
+
+//     },
+    
+//     { id:1,name:"zara",email:"abcxyz@gmail.com",
+
+//     },
+
+//    { id:1,name:"ali",email:"123abc@gmail.com",
+
+//    }
+
+// ];
+//   var u_name=prompt("enter name");
+//   var u_id=prompt("enter id");
+//   var by_name_id_search=obj.find(n=>n.name===u_name && n.id===u_id);
+
+//   if(by_name_id_search){
+
+//     console.log("user found");
+//   }
+
+//   else{
+
+//     // obj = [...obj, { id: 4,name:u_name }];
+//     console.log("not user found");
+//   }
+
+// console.log(by_name_id_search);
+
+
+
+
+//
+// var marks=[40,60,89,80];
+
+// for(var i=0;i<marks.length;i++){
+
+// // console.log(marks[i]);
+
+// var sum=sum+marks[i];
+
+
+
+// }
+  
+// console.log(sum);
+   
+
+
+
+// var companies=companiess.slice(1,2);
+// console.log(companies);
+
+// companiess.splice(0,1,"tazzz");
+// console.log(companiess);
+
+// var companiess=["uber","careem","apple","google"];
+// // companiess.forEach(company => {
+    
+
+//     console.log(company);
+
+
+// })
+
+
+//        var replace=companiess.map((c)=>{
+
+//           return "car";
+
+
+//        });
+//     console.log(replace);
+
+
+// // });
+// // map()
+// //filteter
+
+// var obj = [
+    
+//     { id:1,name:"tazzakka arif",email:"abc@gmail.com",
+
+//     },
+    
+//     { id:2,name:"zara",email:"abcxyz@gmail.com",
+
+//     },
+
+//    { id:3,name:"ali",email:"123abc@gmail.com",}
+
+// ];  // Define obj as an array
+
+// var u_name = prompt("Enter name");
+// var u_id = prompt("Enter id");
+// console.log(u_name);
+// console.log(u_name);
+
+// var by_name_id_search = obj.find(n => n.name === u_name && n.id == u_id);
+
+// if (by_name_id_search) {
+//   console.log("User found");
+// } 
+
+// if u_name.charAt(0)===u_name.charAt(0).toUpperCase()){
+// console.log("name starts with lower case");
+
+// }
+// else {
+//   console.log("User not found");
+// }
+
+// console.log("result:", by_name_id_search);
+
+
+
+  
